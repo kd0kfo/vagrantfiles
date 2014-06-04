d6b2100532bec7d5f0e70d1c79107aae55170323
David Coss <david@davecoss.com>
2014-06-04 09:51:40 -0500

    Updated datascience definition.

--- puppetbased/manifests/datascience.pp
+++ puppetbased/manifests/datascience.pp
@@ -1,6 +1,10 @@
 node default {
-	class {"rstudio":}
-	class {"ipython":}
+	class {"basicdev":}
+	class {"epel":}
+	class {"pip":}
+	package {["blas-devel", "lapack-devel"]:}
+	class {"rstudio::base":}
+	class {"ipython::base":}
 
 	file {"/etc/motd":
 		content => "This is a server with Data Science Tools build using Vagrant and Puppet",
--- puppetbased/modules/rstudio/manifests/base.pp
+++ puppetbased/modules/rstudio/manifests/base.pp
@@ -6,8 +6,15 @@ class rstudio::base {
 		require => Class["epel"],
 	}
 
-	package {"rstudio-server":
-		install_options => ['--nogpgcheck'],
+	exec {"yum localinstall -y --nogpgcheck /tmp/rstudio-server.rpm":
+		path => "/usr/bin:/usr/sbin:/bin",
 		require => [Class["epel"], Wget::Fetch["http://download2.rstudio.org/rstudio-server-0.98.507-x86_64.rpm"]],
+		unless => "which rstudio-server",
 	}	
+
+	file {"/etc/init.d/rstudio-server":
+		source => "/usr/lib/rstudio-server/extras/init.d/redhat/rstudio-server",
+		require => Exec ["yum localinstall -y --nogpgcheck /tmp/rstudio-server.rpm"],
+		ensure => present,
+	}
 }
