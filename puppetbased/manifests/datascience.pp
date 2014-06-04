99800b660ab7f24c2d85718bb00e6fb30332a961
David Coss <david@davecoss.com>
2014-06-04 08:23:58 -0500

    Created a new node definition that combines ipython and rstudio.

---
+++ puppetbased/manifests/datascience.pp
@@ -0,0 +1,8 @@
+node "datascience", /datascience\..+/ {
+	class {"rstudio":}
+	class {"ipython":}
+
+	file {"/etc/motd":
+		content => "This is a server with Data Science Tools build using Vagrant and Puppet",
+		ensure => present,
+	}
