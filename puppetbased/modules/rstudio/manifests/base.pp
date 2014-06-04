7a7997a2f2cf892524b0caab22348145a2d24eac
David Coss <david@davecoss.com>
2014-06-04 09:23:51 -0500

    Added wget download of rpm.

--- puppetbased/modules/rstudio/manifests/base.pp
+++ puppetbased/modules/rstudio/manifests/base.pp
@@ -1,10 +1,13 @@
 class rstudio::base {
+	wget::fetch {"http://download2.rstudio.org/rstudio-server-0.98.507-x86_64.rpm":
+		destination => '/tmp/rstudio-server.rpm',
+	}
 	package {"R-devel":
 		require => Class["epel"],
 	}
 
 	package {"rstudio-server":
 		install_options => ['--nogpgcheck'],
-		require => Class["epel"],
+		require => [Class["epel"], Wget::Fetch["http://download2.rstudio.org/rstudio-server-0.98.507-x86_64.rpm"]],
 	}	
 }
