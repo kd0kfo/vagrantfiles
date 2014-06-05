77e838b92d22d149d9abbabc1f02d8b520c3f4f8
David Coss <david@davecoss.com>
2014-06-05 14:25:32 -0500

    Rearranged prereqs for datascience node.

--- puppetbased/manifests/datascience.pp
+++ puppetbased/manifests/datascience.pp
@@ -1,10 +1,18 @@
-node default {
+class datascienceprereq {
 	class {"basicdev":}
 	class {"epel":}
 	class {"pip":}
 	package {["blas-devel", "lapack-devel"]:}
-	class {"rstudio::base":}
-	class {"ipython::base":}
+}
+
+node default {
+	class {"datascienceprereq":}
+	class {"rstudio::base":
+		require => Class["datascienceprereq"],
+	}
+	class {"ipython::base":
+		require => Class["datascienceprereq"],
+	}
 
 	file {"/etc/motd":
 		content => "This is a server with Data Science Tools build using Vagrant and Puppet",
