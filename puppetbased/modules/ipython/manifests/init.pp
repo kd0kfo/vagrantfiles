f8cf2fdb4fe33784c79dceeb6a7d1f26e1ea2fb4
David Coss <david@davecoss.com>
2014-06-06 12:41:58 -0500

    Added prereqs to ipython/init.pp

--- puppetbased/modules/ipython/manifests/init.pp
+++ puppetbased/modules/ipython/manifests/init.pp
@@ -3,7 +3,17 @@ class ipython {
 	class {"basicdev":}
 	package {["python-matplotlib", "python-gtkextra",  "elinks"]:
 		require => Class["basicdev"],
+	} -> 
+	package {"numpy >= 1.8.1":
+		provider => "pip",
+	} ->
+	package {"scipy":
+		provider => "pip",
+	} ->
+	package {["pandas", "openpyxl<2.0.0"]:
+		provider => "pip",
 	}
+
 	
 	package {["blas-devel", "lapack-devel"]:}
 
