4d94d5c35b6ce5bb7919be0098a260790389c41d
David Coss <david@davecoss.com>
2014-09-27 12:03:44 -0400

    Updated webbox class for changes to basicdev class.

--- puppetbased/modules/webbox/manifests/init.pp
+++ puppetbased/modules/webbox/manifests/init.pp
@@ -1,7 +1,7 @@
 class webbox {
 	class {"basicdev":}
 	
-	package {["httpd", "php", "php-devel", "php-pdo", "sqlite", "postgresql-server", "perl", "python", "python-devel", "vim-enhanced", "emacs", "elinks"]:
+	package {["httpd", "php", "php-devel", "php-pdo", "sqlite", "postgresql-server", "perl", "elinks"]:
 		require => Class["basicdev"]
 	}
 
