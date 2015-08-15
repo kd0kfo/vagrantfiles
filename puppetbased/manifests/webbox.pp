26e339e54e8e7790b1468a1a50340aa34ea5378d
David Coss <david@davecoss.com>
2015-08-15 12:58:09 -0400

    Added missing line return.

--- puppetbased/manifests/webbox.pp
+++ puppetbased/manifests/webbox.pp
@@ -2,7 +2,7 @@ node default {
 	class{"webbox":}
 
 	file {"/etc/motd":
-		content => "This machine was setup using vagrant and puppet to be a web server.",
+		content => "This machine was setup using vagrant and puppet to be a web server.\n",
 		ensure => present,
 	}
 
